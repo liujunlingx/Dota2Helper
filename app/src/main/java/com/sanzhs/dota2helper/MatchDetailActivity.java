95200ca68097738e316e21a90475593b61b5ecc7
sanzhs <39252383@qq.com>
2017-09-05 12:53:05 +0800

    完成查看单场详细功能

---
+++ app/src/main/java/com/sanzhs/dota2helper/MatchDetailActivity.java
@@ -0,0 +1,10 @@
+package com.sanzhs.dota2helper;
+
+import android.app.Activity;
+
+/**
+ * Created by sanzhs on 2017/9/4.
+ */
+
+public class MatchDetailActivity extends Activity {
+}
---
+++ app/src/main/java/com/sanzhs/dota2helper/adapter/PlayerDetailAdapter.java
@@ -0,0 +1,8 @@
+package com.sanzhs.dota2helper.adapter;
+
+/**
+ * Created by sanzhs on 2017/9/4.
+ */
+
+public class PlayerDetailAdapter {
+}
